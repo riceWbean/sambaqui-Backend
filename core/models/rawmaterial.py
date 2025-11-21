2c67d6368526dfb569c15433279aa4a82b0aeb67
João Victor Portela Rocha <131203244+Joaovictor23t4@users.noreply.github.com>
2025-11-21 10:20:50 -0300

    Reduce max_length of RawMaterial name field to 50

--- core/models/rawmaterial.py
+++ core/models/rawmaterial.py
@@ -1,5 +1,5 @@
 from django.db import models
 class RawMaterial(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=50)
     def __str__(self):
         return self.name
