c40e9ee4938145c03ee7d516fd0347389d17b974
João Victor Portela Rocha <131203244+Joaovictor23t4@users.noreply.github.com>
2025-11-21 10:21:26 -0300

    Reduce max_length of name field in Subtype model

--- core/models/subtype.py
+++ core/models/subtype.py
@@ -2,7 +2,7 @@ from django.db import models
 from rawmaterial import RawMaterial
 
 class Subtype(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=50)
     raw_material = models.ForeignKey(RawMaterial, on_delete=models.PROTECT, related_name='subtypes')
 
     def __str__(self):
